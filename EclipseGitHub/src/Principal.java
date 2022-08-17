b4effbec845ddf0363c6655af13b6b8abb0158bd
rodrigo.bampi <rodrigo.bampi@GMEPN002488.metainf.com.br>
2022-08-16 22:35:21 -0300

    Upload

--- EclipseGitHub/src/Principal.java
+++ EclipseGitHub/src/Principal.java
@@ -2,6 +2,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println("Alo Mundo Git Hub, download do Git!!!");
 		System.out.println("Alo Mundo Git Hub, upload do Git!!!");
 	}
 }
